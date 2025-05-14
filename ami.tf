0ff99c8f742ff4108d2d72e6eca3b8b5d45296b8
takuhiro-kaneko <takuhiro.kaneko@kk-grp.jp>
2025-05-14 11:17:57 +0900

    initial commit

---
+++ ami.tf
@@ -0,0 +1,9 @@
+# Definition of ami ver dev 
+data "aws_ami" "windows-2022" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-English-Full-Base*"]
+  }
+}
